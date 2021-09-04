7a3b4698bc06f3ece46c06a67fb6df455e36a8c3
one <wrcj12138@163.com>
2021-09-04 11:58:25 +0800

    IU-2021.2.1 <one@192.168.0.107 Create markdown.xml, IntelliLang.xml, print.xml, jQuery.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Enum.java, Interface.java, ActionScript.xml, COMMENTS.xml, yiiguxing.translation.xml, GSP.xml, Default.xml, find.xml, Darcula copy2.icls, AndroidComments.xml, code.style.schemes.xml, AndroidLog.xml, JSP.xml, Web Services.xml, Zen HTML.xml, Default.xml, RESTful Web Services.xml, Test.xml, scala.xml, JavaScript.xml, AndroidParcelable.xml, lightEdit.xml, Kotlin.xml, maven.wrapper.mapping.xml, plain.xml, one.icls, Android.xml, keymap.xml, XML.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, vim_settings.xml, ide.general.xml, ABC_ONE.xml, other.xml, ide-features-trainer.xml, advancedSettings.xml, lombok-plugin.xml, git.xml, profiler.xml, shared-indexes.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, xsl.xml, plugin_ui.xml, colors.scheme.xml, OneE.xml, notifications.xml, scala_config.xml, databaseDrivers.xml, github.xml, templates.xml, Groovy.xml, laf.xml, emmet.xml, One.xml, parameter.hints.xml, AndroidXML.xml, Class.java, jdk.table.xml, ColdFusion.xml, Jsp File.jsp, intentionSettings.xml, Default _1_.xml, web-browsers.xml, abbreviations.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, usageView.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ *  ${CLASS_INFO}
+ *
+ * @author ${USER}
+ * @version 1.0.0
+ * @since ${YEAR}/${MONTH}/${DAY}
+ */ 
+public interface ${NAME} {
+}
